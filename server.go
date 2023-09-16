a9769a2dcc3a507ef517d4fffae5e261667adfaa
kisihara.c <kisihara.c@gmail.com>
2023-09-16 19:07:22 +0900

    文字列を返すところまで

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"io"
+	"fmt"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,11 +15,31 @@ func main() {
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
 
-  e.GET("/", hello)
+  e.GET("/", dog)
+  e.GET("/hello", hello)
 
   e.Logger.Fatal(e.Start(":1323"))
 }
 
 func hello(c echo.Context) error {
   return c.String(http.StatusOK, "Hello, World!")
+}
+
+func dog(c echo.Context) error {
+	res, err := http.Get("https://dog.ceo/api/breeds/image/random")
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	
+  fmt.Printf(string(data))
+	return c.JSON(http.StatusOK, string(data))
+
+	// 画像を表示
+	// return c.File(http.StatusOK, file)
 }
